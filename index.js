eb44b266a57a80d6a7809b2126669307b2c22680
Victor Tran <vtranwd@gmail.com>
2021-05-30 21:56:58 -0700

    implemented get handler

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 const http = require('http');
+const hostname = '127.0.0.1'; //Ping back address
+const port = 3000;
 
-console.log("hello world");
+
+const server = http.createServer((req, res) => {
+	switch (req.method.toLowerCase()) {
+		case 'put':
+			handlePut();
+			break;
+		case 'get':
+			handleGet(req, res);
+			break;
+		case 'post':
+			handlePost();
+			break;
+		case 'delete':
+			handleDelete();
+			break;
+		default:
+			burnItAll(res);
+			break;
+	}
+});
+
+function handlePut() {}
+
+function handleGet(req, res) {
+	switch(req.url) {
+		case '/foo':
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			const myData = { thing: 'mahBooo' };
+			const payload = JSON.stringify(myData);
+			res.end(payload);
+			break;
+		default:
+			burnItAll(res);
+			break;
+	}
+}
+function handlePost() {}
+
+function handleDelete() {}
+
+function burnItAll(res) {
+	res.statusCode = 500;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Encountered unhandled HTTP Method ${req.method}');
+}
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
